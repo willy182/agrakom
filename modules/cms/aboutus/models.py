dd382df2d2874a0c15519e97bc4437818c829f5b
Muhamad Rusdi Syahren <rusdi.syahen@bhinneka.com>
2018-05-05 15:14:43 +0700

    CMS : bug fix model because upgrade django version

--- modules/cms/aboutus/models.py
+++ modules/cms/aboutus/models.py
@@ -10,9 +10,9 @@ class AboutUs(models.Model):
     title = models.CharField(max_length=225)
     description = models.TextField(blank=True, null=True)
     status = models.BooleanField(blank=True)
-    created_by = models.ForeignKey(User, blank=True, null=True, db_column='created_by', related_name='%(class)s_created_by')
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING, db_column='created_by', related_name='%(class)s_created_by')
     created_datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    modified_by = models.ForeignKey(User, blank=True, null=True, db_column='modified_by', related_name='%(class)s_modified_by')
+    modified_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING, db_column='modified_by', related_name='%(class)s_modified_by')
     modified_datetime = models.DateTimeField(auto_now=True, blank=True, null=True)
 
 
@@ -20,13 +20,12 @@ class SliderAboutUs(models.Model):
     class Meta:
         db_table = 'slider_about_us'
 
-    about_us = models.ForeignKey(AboutUs)
-    position =  models.IntegerField(blank=True, null=True)
+    about_us = models.ForeignKey(AboutUs,on_delete=models.DO_NOTHING)
+    position = models.IntegerField(blank=True, null=True)
     image = models.FileField(blank=True, null=True, upload_to='images/slider')
-    caption =  models.CharField(max_length=225,blank=True, null=True)
+    caption = models.CharField(max_length=225, blank=True, null=True)
     status = models.BooleanField(blank=True)
-    created_by = models.ForeignKey(User, blank=True, null=True, db_column='created_by', related_name='%(class)s_created_by')
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING,db_column='created_by', related_name='%(class)s_created_by')
     created_datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    modified_by = models.ForeignKey(User, blank=True, null=True, db_column='modified_by', related_name='%(class)s_modified_by')
+    modified_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING, db_column='modified_by', related_name='%(class)s_modified_by')
     modified_datetime = models.DateTimeField(auto_now=True, blank=True, null=True)
-
--- modules/cms/ourclients/models.py
+++ modules/cms/ourclients/models.py
@@ -11,12 +11,9 @@ class Ourclient(models.Model):
     description = models.TextField(blank=True, null=True)
     image = models.FileField(blank=True, null=True, upload_to='images/client')
     caption = models.CharField(max_length=225, blank=True, null=True)
-    position =  models.IntegerField(blank=True, null=True)
+    position = models.IntegerField(blank=True, null=True)
     status = models.BooleanField(blank=True)
-    created_by = models.ForeignKey(User, blank=True, null=True, db_column='created_by', related_name='%(class)s_created_by')
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING, db_column='created_by', related_name='%(class)s_created_by')
     created_datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    modified_by = models.ForeignKey(User, blank=True, null=True, db_column='modified_by', related_name='%(class)s_modified_by')
+    modified_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING, db_column='modified_by', related_name='%(class)s_modified_by')
     modified_datetime = models.DateTimeField(auto_now=True, blank=True, null=True)
-
-
-
