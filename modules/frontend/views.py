3e338b2bccbf17fa705be5dfb59986a0869cc280
willy kristianto <willykristianto89@gmail.com>
2018-06-01 01:15:20 +0700

    bug fix email

--- modules/frontend/views.py
+++ modules/frontend/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail
 from django.shortcuts import render
 from django.utils.text import slugify
